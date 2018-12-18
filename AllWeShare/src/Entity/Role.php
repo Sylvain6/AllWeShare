70e1b4658a434a66b7fc89b0723f2881447b2019
Jonathan Rakotonirina <jrakotonirina.ext@wedigital.garden>
2018-12-18 10:27:04 +0100

    Docker connection with PostgreSQL + Test Creation Entity

---
+++ AllWeShare/src/Entity/Role.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RoleRepository")
+ */
+class Role
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $rolename;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRolename(): ?string
+    {
+        return $this->rolename;
+    }
+
+    public function setRolename(string $rolename): self
+    {
+        $this->rolename = $rolename;
+
+        return $this;
+    }
+}
