8324336662b8bc64982017475dcf9135cdc2f475
Antoine Dumont <antoine.dumont@finelia.fr>
2019-01-25 12:34:05 +0100

    Ajout du Save dans account + intégration navbar onglet actif

--- AllWeShare/src/Controller/Front/UserController.php
+++ AllWeShare/src/Controller/Front/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Front;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Form\UserAccountType;
 use App\Repository\UserRepository;
 use Egulias\EmailValidator\Exception\ExpectingDomainLiteralClose;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,6 +13,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/*
+ * Uses for accountForm
+ */
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 /**
  * @Route("/user")
@@ -52,15 +58,26 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("/account", name="user_account", methods={"GET"})
-     * @param UserRepository $userRepository
-     * @return Response
+     * @Route("/account", name="user_account", methods={"GET", "POST"})
      */
-    public function account( Request $request, UserRepository $userRepository ): Response
+    public function account( Request $request, UserPasswordEncoderInterface $encoder ): Response
     {
         $user = $this->getUser();
-        //throw new Exception( json_encode( $user ) );
-        return $this->render('Front/user/account.html.twig', ['user' => $user ]);
+        $form = $this->createForm(UserAccountType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //$encoded = $encoder->encodePassword($user_informations, $user_informations->getPassword());
+            //$user_informations->setPassword($encoded);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_account');
+        }
+
+        return $this->render('Front/user/account.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
 
     }
 }
